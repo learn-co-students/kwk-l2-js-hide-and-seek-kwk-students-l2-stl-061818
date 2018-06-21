396d1ab7b05a3d8a850dbfac436fd45b97a74391
Scarlett Patton <scarwars05@gmail.com>
2018-06-21 21:18:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //Code your solution here
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget(target) {
+  return document.querySelector('.target');
+}
+
+function increaseRankBy(n) {
+const rank = document.querySelectorAll('ul.ranked-list');
+
+for (let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = (i + 1).toString();
+  }
+}
+function deepestChild(child) {
+  return document.querySelector('#grand-node div div div div');
+}
